29aa4627a2927893d1cd6e9156be865a6ea298fa
KP Kaiser <kirk@zothcorp.com>
2023-02-28 19:28:49 +0000

    let's use click instead for interactive prompt

--- cli.py
+++ cli.py
@@ -1,9 +1,9 @@
-import typer
+import click
 
+@click.command()
+@click.option('--version', prompt=True)
+def choose_version(version):
+    print(f"verion is {version}")
 
-def main(name: str):
-    print(f"Hello {name}")
-
-
-if __name__ == "__main__":
-    typer.run(main)
+if __name__ == '__main__':
+    choose_version()
